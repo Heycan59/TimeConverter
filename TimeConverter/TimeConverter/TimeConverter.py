b5411d531fd5b8738eb4cfe1646528a2368ad34c
Hey_Can <86666087+Heycan59@users.noreply.github.com>
2023-05-24 19:06:23 +0800

    Delete TimeConverter directory

--- TimeConverter/TimeConverter/TimeConverter.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime, timezone, timedelta
-
-class TimeConverter:
-    def timelog():
-        dt1 = datetime.utcnow().replace(tzinfo=timezone.utc)
-        dt2 = dt1.astimezone(timezone(timedelta(hours=8)))
-        now = dt2.strftime("%Y-%m-%d %H:%M:%S")
-        return now
