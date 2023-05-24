3cdc9ef5fb03747061f84b4cd45ba8bec5c92845
Hey_Can <86666087+Heycan59@users.noreply.github.com>
2023-05-24 19:06:01 +0800

    Add files via upload

---
+++ TimeConverter/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='TimeConverter',
+    version='1.0.0',
+    author='罐頭Hey_Can#0323',
+    author_email='liooe59@gmail.com',
+    description='A time conversion package',
+    packages=['TimeConverter'],
+    install_requires=[],
+)
